e86522105d27d48c15ad9ef1da9cd3ec8ace6dcd
ihsainh <ihsan.inh@gmail.com>
2019-09-09 15:07:04 +0700

    refactoring file

--- src/screens/Home/components/BoostFeature.js
+++ src/screens/Home/components/BoostFeature.js
@@ -1,44 +1,41 @@
-import React from 'react';
-import { View, Image, FlatList, StyleSheet, Text } from 'react-native';
+import React from "react";
+import { View, Image, FlatList, StyleSheet, Text } from "react-native";
 
 const BoostFeature = props => {
-	const featureLists = [
-		{
-			image: require("../../../assets/img/reward.jpg"),
-			title: 'Get Lucky'
-		},
-		{
-			image: require("../../../assets/img/invite.jpg"),
-			title: 'Invite'
-		},
-		{
-			image: require("../../../assets/img/medicall.jpg"),
-			title: 'MediCall'
-		},
-		{
-			image: require("../../../assets/img/boostquest.jpg"),
-			title: 'BoostQuest'
-		}
-	]
-    return (
-      <View style={styles.cardFeature}>
-        <View style={styles.featureContentWrapper}>
-		      {featureLists.map((item, i) => (
-		      	<View style={styles.featureContent} key={i}>
-				      <Image
-				        style={styles.featureContentImg}
-				        source={item.image}
-				      />
-				      <Text style={styles.featureContentLabel}>{item.title}</Text>
-				    </View>
-		      ))}
-        </View>
+  const featureLists = [
+    {
+      image: require("../../../assets/img/reward.jpg"),
+      title: "Get Lucky"
+    },
+    {
+      image: require("../../../assets/img/invite.jpg"),
+      title: "Invite"
+    },
+    {
+      image: require("../../../assets/img/medicall.jpg"),
+      title: "MediCall"
+    },
+    {
+      image: require("../../../assets/img/boostquest.jpg"),
+      title: "BoostQuest"
+    }
+  ];
+  return (
+    <View style={styles.cardFeature}>
+      <View style={styles.featureContentWrapper}>
+        {featureLists.map((item, i) => (
+          <View style={styles.featureContent} key={i}>
+            <Image style={styles.featureContentImg} source={item.image} />
+            <Text style={styles.featureContentLabel}>{item.title}</Text>
+          </View>
+        ))}
       </View>
-    );
-}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-	cardFeatures: {
+  cardFeatures: {
     marginHorizontal: 20,
     height: 80,
     width: "100%",
@@ -63,7 +60,7 @@ const styles = StyleSheet.create({
   featureContentLabel: {
     color: "#797779",
     fontSize: 12
-  },
+  }
 });
 
 export default BoostFeature;

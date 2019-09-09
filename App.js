ccd5cdf18f58923f4d2e162c6d2c955e4e832f8a
ihsainh <ihsan.inh@gmail.com>
2019-09-09 13:42:29 +0700

    refactoring code

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-          <AppWithNavigationState />
+        <AppWithNavigationState />
       </Provider>
     );
   }
